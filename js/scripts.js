f6c1950d9d6cd77d35b7c61610a819e25c23ec20
Svea Wade <svealinneawade@gmail.com>
2020-11-08 12:49:31 -0800

    trying to find out the bug

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,6 @@
 
 //Test: It should correctly return the price of Pizza based on the toppings and size
 //Expect: Pizza(Pepperoni, Large, Olives).toEqual($21.50)
-
 //Business Logic for Pizzas
 
 function Pizza(meat, cheese, toppings, size) {
@@ -14,34 +13,37 @@ function Pizza(meat, cheese, toppings, size) {
   this.cheese = cheese;
   this.toppings = toppings;
   this.size = size;
-  this.price =0;
+  this.price;
 }
 
 
 Pizza.prototype.totalPrice = function() {
+  this.price = 0;
+  console.log(this.size)
   if (this.size === "Large" || "Medium") {
-    this.price = 20;
+    this.price += 25;
   } else if (this.size === "Small") {
-    this.price = 15;
+    this.price += 15;
   }
-
+  console.log(this.toppings)
   if (this.toppings == "onion" || "tomato" || "bell pepper" || "mushroom" || "pineapple") {
     this.price += .50
   } else if (this.toppings == "roasted garlic" || "olives" || "pineapple") {
     this.price += .75
   }
-
+  console.log(this.meat)
   if (this.meat == "pepperoni" || "chicken" || "sausage" || "bacon") {
     this.price += 1.00
   } else if (this.meat ==="steak") {
     this.price += 2.50
   }
-
+  console.log(this.cheese)
   if (this.cheese == "cheddar" || "mozzarella" || "parmesan") {
     this.price += 1.50
   } else if (this.cheese === "gouda" || "goat cheese") {
     this.price += 2
   }
+  console.log(this.price)
   return this.price
 }
 
@@ -53,13 +55,14 @@ $(document).ready(function(){
     
     let size = $('#size').val();
     let meat = $('#meat').val();
-    let toppings = $('input[name=toppings]:checked').val();
+    let toppings = $('input[name=spinach]:checked').val();
     let cheese = $('#cheese').val();
  
     let thePizza = new Pizza(meat, cheese, toppings, size)
 
     total = thePizza.totalPrice();
     console.log(thePizza)
+    //$('#total').text("$" + total)
     $('#total').text("$" + total)
   });
 });
